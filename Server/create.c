5f4d0f368c0936102ab4b71111c361db7b9e9de6
Minemew2 <leandre.mc2@gmail.com>
2023-06-09 15:55:00 +0200

    Debut de l'arbre

--- Server/create.c
+++ Server/create.c
@@ -1,29 +1,94 @@
 #include "create.h"
-#include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+Film * createMovie(char * name, char * realisatorName, char * categoryName, int duration){
+    Film * newFilm = malloc(sizeof(Film));
+    newFilm->name = name;
+    newFilm->category = categoryName;
+    newFilm->realisator = realisatorName;
+    // strcpy(newFilm->name, name);
+    // strcpy(newFilm->realisator, realisatorName);
+    // strcpy(newFilm->category, categoryName);
+    newFilm->time = duration;
+
+    return newFilm;
+}
+
+void printMovie(Film * movie){
+    printf("Nom : %s\n, Realisateur : %s\n, categorie : %s\n, duree : %d\n", movie->name, movie->realisator, movie->category, movie->time);
+}
+
+Node_realisator * initNode(char character){
+    Node_realisator * result = malloc(sizeof(Node_realisator));
+    result->childsNumber = 0;
+    result->character = character;
+    result->isName = 0;
+    result->childs = NULL;
+    return result;
+}
+
+void addWord(Node_realisator * root, char * word, Realisator * associateRealisator){
+    char actual_char  = 0;
+    int isExisting = 0;
+    Node_realisator * parcours = root;
+    char * comparator = malloc(sizeof(char)*strlen(word));
+
+    for(int i = 0; i<strlen(word) ; i++){
+        actual_char = word[i];
+
+        for(int j = 0; j< parcours->childsNumber; j++){
+            if(parcours->childs != NULL){
+                printf("hello : %c\n", actual_char);
+                if(parcours->childs[j]->character == actual_char){
+                    printf("hello : %c\n", actual_char);
+                    isExisting = 1;
+                    parcours = parcours->childs[i];
+                }
+            }
+        }
+        if(isExisting == 0){
+            printf(" childsNumber : %d\n", parcours->childsNumber);
+            parcours->childsNumber++;
+            parcours->childs = realloc(parcours->childs, parcours->childsNumber*sizeof(Node_realisator*));
+            parcours->childs[parcours->childsNumber-1] = initNode(actual_char);
 
 
-Node_realisator *createRealisatorTree(char * nom_fichier_csv){ //C'est un test qu'il faudra supprimer avant d'envoyer le code
-    Node_realisator *newRealisator = 0;
-    char *realisateur = "Tarantino";
-    newRealisator -> character = *realisateur;
 
+            parcours = parcours->childs[parcours->childsNumber-1];
+        }
+        isExisting = 0;
+
+        comparator[i] = actual_char;
+    }
+
+    if(strcmp(comparator, word) == 0){
+        parcours->isName = 1;
+        parcours->associateRealisator  = associateRealisator;
+    }
+}
+
+void printTree(Node_realisator * root){
+    if(root != NULL){
+        printf("%c\n", root->character);
+        if(root->associateRealisator != NULL){
+            printRealisator(root->associateRealisator);
+        }
+        for(int i = 0; i < root->childsNumber; i++){
+            printTree(root->childs[i]);
+        }
+    }
+}
+
+Realisator * createRealisator(char * name, int movieNumber){
+    Realisator * newRealisator = malloc(sizeof(Realisator));
+    newRealisator->name = name;
+    newRealisator->nbrMovies = movieNumber;
+    newRealisator->movies = malloc(sizeof(Film)*movieNumber);
     return newRealisator;
 }
 
-Realisator **createByMoviesNumber(char * nom_fichier_csv){
-
-    Realisator **newMovieNumber = malloc(sizeof(Realisator*)*3);
-    newMovieNumber[0] = malloc(sizeof(Realisator));
-    newMovieNumber[0] -> name = "Tarantino";
-    newMovieNumber[0] -> nbrMovies = 5;
-
-    newMovieNumber[1] = malloc(sizeof(Realisator));
-    newMovieNumber[1] -> name = "Lysandre";
-    newMovieNumber[1] -> nbrMovies = 2;
-    
-    newMovieNumber[2] = malloc(sizeof(Realisator));
-    newMovieNumber[2] -> name = "Gwendal";
-    newMovieNumber[2] -> nbrMovies=3;
-    return newMovieNumber;
+void printRealisator(Realisator * real){
+    printf("Nom : %s, nombre de films : %d\n", real->name, real->nbrMovies);
 }
--- Server/main.c
+++ Server/main.c
@@ -5,11 +5,11 @@
 
 
 int main(){
-    char *nameTest = "Bonjour";
-    printf("%s\n",nameTest);
-    Realisator **byMoviesNumber = createByMoviesNumber(nameTest);
-    Realisator *bestRealisator = topRealisator(byMoviesNumber);
-    printf("\n%s\n\n",bestRealisator->name);
-    printf("Hello world !\n");
+    Realisator * real = createRealisator("tou", 3);
+    Realisator * real2 = createRealisator("tab", 4);
+    Node_realisator * root = initNode('R');
+    addWord(root, "tou", real);
+    addWord(root, "tab", real2);
+    printTree(root);
     return 0;
 }
