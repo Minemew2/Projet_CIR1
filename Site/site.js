02377b1688489207f6e478f4f90ad7f9d6022e91
Skyl1um <skilium29@gmail.com>
2023-06-12 14:06:50 +0200

    début js

---
+++ Site/site.js
@@ -0,0 +1,12 @@
+function donnees() {
+    var real = document.getElementById("real").value;
+    var duree = document.getElementById("duree").value;
+    var categorie = document.getElementById("categorie").value;
+
+    console.log(real)
+    console.log(duree)
+    console.log(categorie)
+}
+
+
+
