62b7880fed68c783c32615a143a6b48d071f1d21
Skyl1um <skilium29@gmail.com>
2023-06-13 11:25:02 +0200

    nouveaux changements

--- Site/search.js
+++ Site/search.js
@@ -1,6 +1,25 @@
 let _box = document.createElement("div");
-var tab = new Array();
-tab[0] = 'Index';
-tab[1] = 'Réalisateur';
-tab[2] = 'Titre';
-tab[3] = 'Catégorie';
+
+let nbfilms=3;
+let titre=["Titanic","Avatar","Indiana Jones"];
+let realisator=["James Cameron","James Cameron","Steven Spielberg"];;
+let duree=["120","180","90"];;
+
+for (let i = 0; i < nbfilms; i++) {
+    ajouterLigne(i,titre[i],realisator[i],duree[i]);
+}
+
+
+function ajouterLigne(index,titre,realisateur,duree)
+{
+	var tableau = document.getElementById("tableau");
+	let row=document.tableau.createElement("tr");
+    let col1=document.row.createElement("th");
+    col1.innerhtml=index;
+    let col2=document.row.createElement("th");
+    col2.innerhtml=titre;
+    let col3=document.row.createElement("th");
+    col3.innerhtml=realisateur;
+    let col4=document.row.createElement("th");
+    col4.innerhtml=duree;
+}
