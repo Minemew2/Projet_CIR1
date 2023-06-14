cb6d6cbc8bdbfa2780017d438732767786b43869
Minemew2 <leandre.mc2@gmail.com>
2023-06-14 11:52:23 +0200

    comm work

---
+++ Server/delete.h
@@ -0,0 +1,9 @@
+#include "create.h"
+
+
+void freeMovie(Film * movie);
+void freeFilmList(Film_List * list);
+void freeRealisator(Realisator * real);
+void freeTree(Node_realisator * root);
+void freeByMoviesNumber(byMovieNumber * array);
+void freeByDuration(Film_List ** byDuration);
--- Server/main.c
+++ Server/main.c
@@ -9,7 +9,7 @@
 
 
 int main(int argc, char **argv){
-    FILE * request = fopen("request.txt", "r");
+    FILE * request = fopen(argv[2], "r");
 
     Node_realisator * trueTree = initNode('0');
     Realisator * root = createRealisator("byMovieNumberRoot", -1);
@@ -25,14 +25,17 @@ int main(int argc, char **argv){
     int * mustExit = malloc(sizeof(int));
 
     do{
-        request = fopen("request.txt", "r");
+        request = fopen(argv[2], "r");
         if(request != NULL){
-            readRequest(request, trueTree, ranking, byDuration, mustExit);
+            readRequest(request, trueTree, ranking, byDuration, mustExit, argv[2]);
         } else {
             fclose(request);
         }
         
     } while (!(*mustExit));
+
+    remove(argv[2]);
+    free(mustExit);
     
     
     return 0;
